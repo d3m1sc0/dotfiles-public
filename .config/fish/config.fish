134d0c804c4bdf4b3097bc2896bde33fe8b1fa20
Demetris Mina <demi@demetriss-macbook-pro.home>
2022-01-15 17:44:24 +0100

    first commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g theme_nerd_fonts yes
     set -g theme_color_scheme solarized
+
+    alias vim nvim
 end
 
